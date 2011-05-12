86f99f1e3ffbab4267a7cc9a327897ac0d5d6ee8
Jeremy Stephens <viking415@gmail.com>
2011-05-12 16:06:00 -0500

    Get coupler from a rubygems repository instead

--- src/main/resources/lib/coupler/launcher.rb
+++ src/main/resources/lib/coupler/launcher.rb
@@ -1,19 +1,72 @@
 require 'java'
 require 'rbconfig'
-require 'date'
-require 'uri'
-require 'digest/md5'
-require 'fileutils'
-require 'net/http'
-require 'net/https'
-require 'pp'
+require 'rubygems'
+require 'rubygems/dependency_installer'
+
+class Gem::ConfigFile
+  # NOTE: Monkey patch ConfigFile so it doesn't load user's gemrc :(
+  def initialize(arg_list)
+    @config_file_name = nil
+    need_config_file_name = false
+
+    arg_list = arg_list.map do |arg|
+      if need_config_file_name then
+        @config_file_name = arg
+        need_config_file_name = false
+        nil
+      elsif arg =~ /^--config-file=(.*)/ then
+        @config_file_name = $1
+        nil
+      elsif arg =~ /^--config-file$/ then
+        need_config_file_name = true
+        nil
+      else
+        arg
+      end
+    end.compact
+
+    @backtrace = DEFAULT_BACKTRACE
+    @benchmark = DEFAULT_BENCHMARK
+    @bulk_threshold = DEFAULT_BULK_THRESHOLD
+    @verbose = DEFAULT_VERBOSITY
+    @update_sources = DEFAULT_UPDATE_SOURCES
+
+    operating_system_config = Marshal.load Marshal.dump(OPERATING_SYSTEM_DEFAULTS)
+    platform_config = Marshal.load Marshal.dump(PLATFORM_DEFAULTS)
+    system_config = load_file SYSTEM_WIDE_CONFIG_FILE
+    #user_config = load_file config_file_name.dup.untaint
+    if ENV['COUPLER_DEV']
+      user_config = { :sources => %w{http://data.vanderbilt.edu/coupler/} }
+    else
+      user_config = {}
+    end
+
+    @hash = operating_system_config.merge platform_config
+    @hash = @hash.merge system_config
+    @hash = @hash.merge user_config
+
+    # HACK these override command-line args, which is bad
+    @backtrace        = @hash[:backtrace]        if @hash.key? :backtrace
+    @benchmark        = @hash[:benchmark]        if @hash.key? :benchmark
+    @bulk_threshold   = @hash[:bulk_threshold]   if @hash.key? :bulk_threshold
+    @home             = @hash[:gemhome]          if @hash.key? :gemhome
+    @path             = @hash[:gempath]          if @hash.key? :gempath
+    @update_sources   = @hash[:update_sources]   if @hash.key? :update_sources
+    @verbose          = @hash[:verbose]          if @hash.key? :verbose
+
+    load_rubygems_api_key
+
+    Gem.sources = @hash[:sources] if @hash.key? :sources
+    handle_arguments arg_list
+  end
+end
+Gem.configuration # force
 
 module Coupler
   class Launcher
     include java.lang.Runnable
     include_package 'java.awt'
 
-    FILES_URL = "http://biostat.mc.vanderbilt.edu/coupler/"
     TEXT_X = 130
     TEXT_Y = 200
     TEXT_W = 260
@@ -46,65 +99,97 @@ module Coupler
     def find_coupler_dir
       print_update("Locating Coupler directory...")
 
-      @coupler_dir =
-        case Config::CONFIG['host_os']
-        when /mswin|windows/i
-          # Windows
-          File.join(ENV['APPDATA'], "coupler")
+      # NOTE: Unfortunately, this code is in two places. Coupler can
+      # be run with or without the launcher, and the launcher needs
+      # to know about Coupler's data path before it runs Coupler.
+      dir =
+        if ENV['COUPLER_HOME']
+          ENV['COUPLER_HOME']
         else
-          if ENV['HOME']
-            File.join(ENV['HOME'], ".coupler")
+          case Config::CONFIG['host_os']
+          when /mswin|windows/i
+            # Windows
+            File.join(ENV['APPDATA'], "coupler")
           else
-            # FIXME: ask the user
-            raise "Can't figure out where Coupler lives"
+            if ENV['HOME']
+              File.join(ENV['HOME'], ".coupler")
+            else
+              raise "Can't figure out where Coupler lives! Try setting the COUPLER_HOME environment variable"
+            end
           end
         end
-      @coupler_dir = File.expand_path(@coupler_dir)
-
-      if !File.exist?(@coupler_dir)
+      if !File.exist?(dir)
         begin
-          Dir.mkdir(@coupler_dir)
-        rescue Errno::EACCES
-          raise "Couldn't create the Coupler directory: #{@coupler_dir}"
+          Dir.mkdir(dir)
+        rescue SystemCallError
+          raise "Can't create Coupler directory (#{dir})! Is the parent directory accessible?"
         end
       end
+      if !File.writable?(dir)
+        raise "Coupler directory (#{dir}) is not writable!"
+      end
+      @coupler_dir = File.expand_path(dir)
       puts "COUPLER DIR: #{@coupler_dir}"
+
+      gems_dir = File.join(@coupler_dir, "gems")
+      Gem.use_paths(gems_dir, [gems_dir])
     end
 
-    def find_latest_files
-      print_update("Checking for updates...")
+    def install_or_update_coupler
+      # Most of this is from what I could figure out by trying to parse
+      # the install and update rubygems commands; a lot of copied code
 
-      doc = org.jsoup.Jsoup.connect(FILES_URL).get
-      elts = doc.select('table tbody tr')
-      raise "Can't connect to repository" unless elts.size > 0
+      needed_gems = %w{coupler}
+      version = Gem::Requirement.default
+      installer_options = {
+        :env_shebang => false, :domain => :remote, :force => false,
+        :format_executable => false, :ignore_dependencies => false,
+        :prerelease => false, :security_policy => nil,
+        :wrappers => false, :generate_rdoc => false,
+        :generate_ri => false, :version => version,
+        :args => needed_gems
+      }
 
-      files = {}
-
-      elts.each do |tr|
-        tds = tr.select('td')
-        file_td = tds.get(0)
-        mtime_td = tds.get(1)
-        md5_td = tds.get(2)
-        link = file_td.select('a').get(0)
-
-        basename = link.html
-        name = basename.sub(/-[a-f0-9]{7}\.jar$/, "")
-        mtime = Date.parse(mtime_td.html)
-
-        if files[name].nil? || files[name][:mtime] < mtime
-          files[name] = {
-            :basename => basename,
-            :mtime => mtime,
-            :href => link.attr('href'),
-            :md5 => md5_td.html
-          }
+      hig = {}  # highest installed gems
+      Gem.source_index.each do |name, spec|
+        if hig[spec.name].nil? or hig[spec.name].version < spec.version then
+          hig[spec.name] = spec
         end
       end
 
-      puts "=== LATEST FILES ==="
-      pp files
+      needed_gems.each do |gem_name|
+        if hig[gem_name].nil?
+          print_update("Installing #{gem_name}...")
 
-      @latest_files = files
+          installer = Gem::DependencyInstaller.new(installer_options)
+          installer.install(gem_name, version)
+        else
+          print_update("Checking #{gem_name}...")
+
+          l_name = gem_name
+          l_spec = hig[gem_name]
+          dependency = Gem::Dependency.new(l_spec.name, "> #{l_spec.version}")
+          fetcher = Gem::SpecFetcher.fetcher
+          spec_tuples = fetcher.find_matching dependency
+
+          matching_gems = spec_tuples.select do |(name, _, platform),|
+            name == l_name and Gem::Platform.match platform
+          end
+
+          if !matching_gems.empty?
+            highest_remote_gem = matching_gems.sort_by do |(_, version),|
+              version
+            end.last
+            highest_remote_ver = highest_remote_gem.first[1]
+
+            if l_spec.version < highest_remote_ver
+              print_update("Updating #{gem_name}...")
+              installer = Gem::DependencyInstaller.new(installer_options)
+              installer.install(l_name, version)
+            end
+          end
+        end
+      end
     end
 
     def setup_progress_bar
@@ -131,105 +216,11 @@ module Coupler
       @splash.update
     end
 
-    def download_file(name, url, local)
-      puts "DOWNLOADING: #{url}"
-      print_update("Downloading #{name}...")
-      http = Net::HTTP.new(url.host, url.port)
-      http.request_get(url.path) do |response|
-        setup_progress_bar
-        out = File.open(local, 'wb')
-        total_size = response['content-length']
-        size = 0
-        response.read_body do |segment|
-          size += segment.length
-          nudge_progress_bar(size, total_size)
-          out.write(segment)
-        end
-        out.flush
-        out.close
-        remove_progress_bar
-      end
-      puts "DOWNLOADED: #{name} => #{local}"
-    end
-
-    def update_installation
-      files_url = URI.parse(FILES_URL)
-      @installed_files = {}
-      puts "=== VERIFY FILES ==="
-      @latest_files.each_pair do |name, info|
-        shortname = name.split(/-/)[-1]
-        print_update("Verifying #{shortname}...")
-
-        get_file = true
-        local_fn = File.join(@coupler_dir, info[:basename])
-        if File.exist?(local_fn)
-          md5 = Digest::MD5.hexdigest(File.open(local_fn, 'rb') { |f| f.read })
-          puts "Local MD5:  #{md5.inspect}"
-          puts "Remote MD5: #{info[:md5].inspect}"
-          if md5 != info[:md5]
-            puts "#{info[:basename]} seems to be corrupt; will redownload it."
-          else
-            puts "#{info[:basename]} looks good to me!"
-            get_file = false
-          end
-        end
-
-        if get_file
-          url = files_url + info[:href]
-          download_file(shortname, url, local_fn)
-        end
-        @installed_files[name] = local_fn
-      end
-
-      # unlink old files
-      old_files = Dir[File.join(@coupler_dir, "*.jar")] - @installed_files.values
-      old_files.each do |fn|
-        FileUtils.rm_f(fn)
-      end
-      puts "INSTALLED FILES: #{@installed_files.inspect}"
-    end
-
     def start_coupler
-      tmp = %w{coupler coupler-dependencies} - @installed_files.keys
-      if !tmp.empty?
-        print_update("Missing #{tmp.join(' and ')} runtime files. Quitting...")
-        sleep 2
-        return false
-      end
-
-      # This involves some skullduggery.
-      dependency_jar = @installed_files['coupler-dependencies']
-      Gem.use_paths(nil, ["file:#{dependency_jar}!/"])
-      require dependency_jar  # for java stuff
-      require "file:#{@installed_files['coupler']}!/lib/coupler"
-
-      if !Coupler::Server.instance.is_running?
-        print_update("Starting database...")
-        @stop_server = true
-        Server.instance.start
-      end
-
-      print_update("Migrating database...")
-      Coupler::Database.instance.migrate!
-
-      if !Coupler::Scheduler.instance.is_started?
-        print_update("Starting scheduler...")
-        @stop_scheduler = true
-        Coupler::Scheduler.instance.start
-      end
-
-      print_update("Starting web server...")
-      handler = Rack::Handler.get('mongrel')
-      settings = Coupler::Base.settings
-
-      # See the Rack::Handler::Mongrel.run! method
-      # NOTE: I don't want to join the server, which is why I'm doing this
-      #       by hand.
-      @web_server = Mongrel::HttpServer.new(settings.bind, settings.port, 950, 0, 60)
-      @web_server.register('/', handler.new(Coupler::Base))
-      @web_thread = @web_server.run
-      Coupler::Base.set(:running, true)
-
+      # FIXME: ensure coupler is installed; it might not be if
+      # for some reason it can't be found in the gem repos
+      require 'coupler'
+      @runner = Coupler::Runner.new([], :trap => false) { |msg| print_update(msg) if @splash }
       true
     end
 
@@ -261,7 +252,7 @@ module Coupler
         trap("INT") do
           shutdown
         end
-        @web_thread.join
+        @runner.join
 
         return
       end
@@ -311,15 +302,7 @@ module Coupler
     end
 
     def shutdown
-      if @stop_scheduler
-        Coupler::Scheduler.instance.shutdown
-      end
-
-      if @stop_server
-        Coupler::Server.instance.shutdown
-      end
-
-      @web_server.stop
+      @runner.shutdown
 
       if @tray
         @tray.remove(@tray_icon)
@@ -329,11 +312,11 @@ module Coupler
     def run
       setup_gui
       find_coupler_dir
-      find_latest_files
-      update_installation
+      install_or_update_coupler
       if start_coupler
         launch_browser
         @splash.close
+        @splash = nil
 
         setup_tray_icon
       end
